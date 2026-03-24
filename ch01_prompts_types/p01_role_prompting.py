7d636e499c4b82a7a698852889b0a29986335e6e
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2026-03-23 21:18:28 -0300

    feat: :sparkles: add prompt types such as first chapter

---
+++ ch01_prompts_types/p01_role_prompting.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+from .utils import print_llm_result
+
+load_dotenv()
+
+system = (
+    "system",
+    """You are a university professor of computer science who is very technical and
+    explain concepts with formal definitions and pseudocode.""",
+)
+
+system2 = (
+    "system",
+    """You are a high school student that is starting learning coding. You are not very
+    technical and you prefer to explain concepts with simple words and examples.""",
+)
+
+user = (
+    "user",
+    "Explain recursion in 50 words.",
+)
+
+chat_prompt = ChatPromptTemplate([system, user])
+chat_prompt2 = ChatPromptTemplate([system2, user])
+
+model = ChatOpenAI(model="gpt-4o")
+result = model.invoke(chat_prompt.format_messages())
+print_llm_result(
+    str(system),
+    result,
+)
+
+result2 = model.invoke(chat_prompt2.format_messages())
+print_llm_result(
+    str(system2),
+    result2,
+)
