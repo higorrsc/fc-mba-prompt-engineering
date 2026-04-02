4bdeb15758625c9009a9085e6ca0e63bb7de7214
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2026-04-02 19:50:59 -0300

    feat: :sparkles: add no expansion enriched prompt

---
+++ ch02_enriched_prompt/p01_no_expansion.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain.prompts import PromptTemplate
+from langchain.schema import StrOutputParser
+
+load_dotenv()
+
+llm = init_chat_model(model="openai:gpt-4o-mini", temperature=0.7)
+
+prompt = PromptTemplate(
+    input_variables=["question"],
+    template=(
+        "You are a technology assistant.\n"
+        "Answer the following question:\n\n"
+        "{question}"
+    ),
+)
+
+chain = prompt | llm | StrOutputParser()
+
+QUESTION = "Explain about the LangChain and LangGraph"
+
+answer = chain.invoke({"question": QUESTION})
+print(answer)
+print(len(answer))
